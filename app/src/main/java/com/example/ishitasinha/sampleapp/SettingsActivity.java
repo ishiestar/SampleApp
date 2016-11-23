24c6b4b09b253d3583da594b2566b6c0ed60cbab
ishitasinha <ishiestar@gmail.com>
2016-11-23 16:52:46 +0530

    added a text resize slider

--- app/src/main/java/com/example/ishitasinha/sampleapp/SettingsActivity.java
+++ app/src/main/java/com/example/ishitasinha/sampleapp/SettingsActivity.java
@@ -1,13 +1,21 @@
 package com.example.ishitasinha.sampleapp;
 
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.util.TypedValue;
 import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    TextView sampleText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +43,58 @@ public class SettingsActivity extends AppCompatActivity {
                 }
             });
         }
+
+        setUpTextResize();
     }
+
+    private static SharedPreferences getSharedPreferences(Context context) {
+        return context.getSharedPreferences
+                (context.getString(R.string.app_name), Context.MODE_PRIVATE);
+    }
+
+    /**
+     * For setting font size
+     *
+     * @param context
+     * @param fontSize
+     */
+    public void setFontSize(Context context, String fontSize) {
+        getSharedPreferences(context).edit().putString("FONT_SIZE", fontSize)
+                .apply();
+    }
+
+    /**
+     * For getting font size
+     *
+     * @param context
+     * @return
+     */
+    public String getFontSize(Context context) {
+        return getSharedPreferences(context).getString("FONT_SIZE", "50");
+    }
+
+    private void setUpTextResize() {
+        String fontSize = getFontSize(getApplicationContext());
+        AppCompatSeekBar textResizeSeekbar = ((AppCompatSeekBar) findViewById(R.id.text_resize_seekbar));
+        sampleText = ((TextView) findViewById(R.id.sample_text));
+        textResizeSeekbar.setProgress((int) Float.parseFloat(fontSize));
+        sampleText.setTextSize(TypedValue.COMPLEX_UNIT_PX, textResizeSeekbar.getProgress());
+        textResizeSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sampleText.setTextSize(TypedValue.COMPLEX_UNIT_PX, progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                setFontSize(getApplicationContext(), String.valueOf(sampleText.getTextSize()));
+            }
+        });
+    }
+
 }
